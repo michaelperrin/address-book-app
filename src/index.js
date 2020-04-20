bbfe9291e4b511d8b37dd4876e61507cb0b7cc24
Michaël Perrin <contact@michaelperrin.fr>
2020-04-20 17:00:36 -0500

    Init UI with Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
+import AddressBook from './AddressBook';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddressBook />
   </React.StrictMode>,
   document.getElementById('root')
 );
