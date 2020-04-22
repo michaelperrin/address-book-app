ae31f7ad1794e0dfc44eb764478f994893715944
Michaël Perrin <contact@michaelperrin.fr>
2020-04-22 16:50:45 -0500

    Revert "Remove useless header"
    This reverts commit 0be444d5fffa6dfc476f65f8b25e9479d6bbd472.

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Header from './Header';
 import UserList from './components/UserList';
 
 const AddressBook = () => (
   <div className="address-book-app">
+    <Header />
+
     <div className="container">
       <UserList />
     </div>
