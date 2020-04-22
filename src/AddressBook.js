0be444d5fffa6dfc476f65f8b25e9479d6bbd472
Michaël Perrin <contact@michaelperrin.fr>
2020-04-22 16:46:11 -0500

    Remove useless header

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Header from './Header';
 import UserList from './components/UserList';
 
 const AddressBook = () => (
   <div className="address-book-app">
-    <Header />
-
     <div className="container">
       <UserList />
     </div>
