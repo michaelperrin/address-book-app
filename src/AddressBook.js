06f0f460388a51c8003bef6606de2a5874231fba
Michaël Perrin <contact@michaelperrin.fr>
2020-05-20 17:45:11 -0500

    Enable Redux dev tools

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -4,15 +4,22 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import GlobalSpinner from './components/GlobalSpinner';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
 const UserList = lazy(() => import('./components/UserList'));
 const Settings = lazy(() => import('./components/Settings'));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+);
 
 const AddressBook = () => (
   <Router>
